4b8892085cfc254471541d873ceb2fefe22c9062
SunliteIV <vn.nguyenduchoang@gmail.com>
2024-06-03 14:31:46 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "",
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blogger.googleusercontent.com",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
